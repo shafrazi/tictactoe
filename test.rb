19b982501eea533940e5bca74752ca37e74ae84b
shafrazi <shafrazi@live.com>
2019-07-11 23:48:01 +0530

    add helper methods, game over logic

--- test.rb
+++
@@ -1,9 +0,0 @@
-#Initially display board with numbers marked in each cell
-#Prompts for player1 to choose X or O
-#Create new Player instance with var player1 and assign the symbol of choice.
-#Player class to have an array where each input should be recorded.
-#Prompts for player1 to choose location in board
-#Marks board with player1 input and display new board
-#Prompts for player2 input
-#Marks board with both players input and displays new board
-#loop until a winning combination and announces player
